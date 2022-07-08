1016e1d6bfc8e6ad8d581f5d8d8744ba05609b50
Jim <omesadev@outlook.com>
2022-07-08 18:59:45 -0400

    Initial Header Component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import { Navbar } from 'react-bootstrap';
+
+const Header= ({ title }) => {
+  return (
+    <Navbar bg="light" variant="light">
+      <Navbar.Brand href="/">{title}</Navbar.Brand>
+    </Navbar>
+  );
+};
+
+export default Header;
