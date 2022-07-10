4e492e3da7354276e29dec07417de5a6d274f379
Jim <omesadev@outlook.com>
2022-07-09 21:47:01 -0400

    Controlled Search Component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,15 @@
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
 
-const Header= ({ title }) => {
+const navbarStyle = {
+  backgroundColor: 'lightblue'
+}
+
+const Header = ({ title }) => {
   return (
-    <Navbar bg="light" variant="light">
-      <Navbar.Brand href="/">{title}</Navbar.Brand>
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+      </Container>
     </Navbar>
   );
 };
---
+++ frontend/src/components/Search.js
@@ -0,0 +1,35 @@
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  Button
+} from 'react-bootstrap';
+
+const Search = ({ word, setWord, handleSubmit }) => {
+  return (
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8}>
+          <Form onSubmit={handleSubmit}>
+            <Row>
+              <Col xs={9}>
+                <Form.Control
+                  type="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)}
+                  placeholder="Search for new image" 
+                />
+              </Col>
+              <Col>
+                <Button type="submit" variant="primary">Search</Button>
+              </Col>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Search;
